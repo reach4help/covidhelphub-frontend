1e92426d3e5412ba5dc6d90a4ed88e9ed597d035
Jenney <ren.jenney@gmail.com>
2021-12-09 19:18:00 -0500

    Add FieldComponentWrapper
    To avoid manually wrapping every FieldComponent in styling div

--- src/components/formik/fieldComponents/FieldComponent.tsx
+++ src/components/formik/fieldComponents/FieldComponent.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Style from '../formikStyle.module.css';
 import {
   textFieldSet,
   checkRadioFieldSet,
@@ -14,14 +15,29 @@ interface Props {
   formField: FormField;
 }
 
+function FieldComponentWrapper(component: JSX.Element | undefined) {
+  if (!component) {
+    return null;
+  }
+  return (
+    <div className={Style.formField}>
+      {component}
+    </div>
+  );
+}
+
 function FieldComponent(props: Props) {
   const { formField } = props;
 
+  let component: JSX.Element | undefined;
+
   if (textFieldSet.has(formField.type)) {
-    return <TextFieldComponent formField={formField} />;
-  }
-  if (checkRadioFieldSet.has(formField.type)) {
-    return <CheckRadioFieldComponent formField={formField} />;
+    component = <TextFieldComponent formField={formField} />;
+  } else if (checkRadioFieldSet.has(formField.type)) {
+    component = <CheckRadioFieldComponent formField={formField} />;
+  } else if (selectFieldSet.has(formField.type)) { return FieldComponentWrapper(component); }
+  if (selectFieldSet.has(formField.type)) {
+    return <SelectComponent formField={formField} />;
   }
   if (selectFieldSet.has(formField.type)) {
     return <SelectComponent formField={formField} />;
