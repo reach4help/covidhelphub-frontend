7a85185ce3fb61e074de036800b50bb93d9e4d88
minurajeeve <minpayy@gmail.com>
2022-01-21 14:26:20 +0530

    Form page modified and FormComponent css modules added

--- src/components/form/FormComponent.tsx
+++ src/components/form/FormComponent.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import './FormComponent.module.css';
+
+const formTypes: {
+  key: string;
+  addr: string;
+}[] = [
+  { key: 'Beneficiaries', addr: '/form/beneficiaries' },
+  { key: 'Volunteers', addr: '/form/volunteers' },
+];
 
 function FormComponent() {
+  // const { url } = useRouteMatch();
   return (
     <div>
-      <Link to="/form/beneficiaries">Beneficiary forms</Link>
-      <br />
-      <Link to="/form/volunteers">Volunteer forms</Link>
+      <h1>Forms</h1>
+      <ul>
+        {formTypes.map((formType) => (
+          <li key={formType.key}>
+            <NavLink to={formType.addr}>{formType.key}</NavLink>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
