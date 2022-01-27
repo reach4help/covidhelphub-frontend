98d82b7178adbd962377e62c79530e32d5b5acd7
Nechir dev <66407817+Nechir-89@users.noreply.github.com>
2022-01-27 10:11:50 -0500

    adding global filtering (Searching)

--- src/react-table/Table.tsx
+++ src/react-table/Table.tsx
@@ -10,54 +10,68 @@ import {
   useSortBy,
   useExpanded,
   usePagination,
+  useGlobalFilter,
 } from 'react-table';
+import GlobalFiltering from './GlobalFiltering';
 
-function Table(Props: any) {
-  const { columns, data } = Props;
+function Table({ columns, data }: { columns: any; data: any }) {
   const {
-    getTableProps, getTableBodyProps, headerGroups, rows, prepareRow,
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    state,
+    setGlobalFilter,
   } = useTable(
     { columns, data },
     useFilters,
     useGroupBy,
+    useGlobalFilter,
     useSortBy,
     useExpanded,
     usePagination,
   );
-
+  const { globalFilter } = state;
   return (
-    <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                {column.render('Header')}
-                {/* <span>
-                      {column.isSorted
+    <>
+      <GlobalFiltering filter={globalFilter} setFilter={setGlobalFilter} />
+      <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render('Header')}
+                  <span>
+                    {
+                      // eslint-disable-next-line no-nested-ternary
+                      column.isSorted
                         ? column.isSortedDesc
                           ? ' 🔽'
                           : ' 🔼'
-                        : ''}
-                    </span> */}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => (
-                <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                        : ''
+                    }
+                  </span>
+                </th>
               ))}
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => (
+                  <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
   );
 }
 
