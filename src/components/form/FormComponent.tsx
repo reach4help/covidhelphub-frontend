09561af234cdfbfff8390f9dc588d4e9a43113ac
minurajeeve <minpayy@gmail.com>
2022-02-02 15:13:44 +0530

    making changes in formcomponent css module

--- src/components/form/FormComponent.tsx
+++ src/components/form/FormComponent.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './FormComponent.module.css';
+import Style from './FormComponent.module.css';
 
 const formTypes: {
   key: string;
@@ -11,13 +11,12 @@ const formTypes: {
 ];
 
 function FormComponent() {
-  // const { url } = useRouteMatch();
   return (
     <div>
       <h1>Forms</h1>
-      <ul>
+      <ul className={Style.form_options_outer}>
         {formTypes.map((formType) => (
-          <li key={formType.key}>
+          <li className={Style.form_options_inner} key={formType.key}>
             <NavLink to={formType.addr}>{formType.key}</NavLink>
           </li>
         ))}
